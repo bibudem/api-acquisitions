52b076872dbe05e596541075817abfe2449254f1
Christian Rémillard <christian.remillard@gmail.com>
2023-01-11 13:37:01 -0500

    wip

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3NzUyNQ/spectral.js?branch_slug=main&token=b132c39e-14aa-404c-9d40-b9050c133c3d";
+export default { extends: ruleset };
